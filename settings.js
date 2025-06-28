9fdd054444a88f0614a91e3f41f2a1c71a197440
icantunderstand1 <icantunderstand13@gmail.com>
2025-06-28 16:30:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~EVRiDQ5C#KYw1-VSa2PaXyw22sY37oI30X1NBSE2nLi1wS9rqRZ8"
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~cMwAjK5B#zbKWcXSGwWCvvObsUZGgyDNXS8SXHA3i8IZhAdDXWgs"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94702128378"
 global.ownername = process.env.OWNERNAME || "Cyber Kavi"
